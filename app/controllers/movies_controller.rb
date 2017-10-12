38481ec67c072163ca816815df51e67491d5c3c8
Ethan White <ewhite47@uncc.edu>
2017-10-12 15:44:53 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,8 +11,14 @@ class MoviesController < ApplicationController
   end
 
   def index
+    if params[:title] == "sort"
+      @movies = Movie.all.order(:title => "ASC")
+      elsif params[:release_date] == "sort"
+      @movies = Movie.all.order(:release_date => "ASC")
+    else
     @movies = Movie.all
   end
+end
 
   def new
     # default: render 'new' template
